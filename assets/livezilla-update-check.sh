4b39a98cdc0de007dd96a8dd33b7bbf3cddc2cac
Christian Pedersen <christian.pedersen@zentura.dk>
2019-10-03 15:52:14 +0200

    Added Version pointer

--- assets/livezilla-update-check.sh
+++ assets/livezilla-update-check.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 VERSION=$(livezilla-version)
+UPGRADE_CHECK=$(livezilla-upgradecheck)
 
-if [ $VERSION != $LIVEZILLA_VERSION ]; then
-    echo "Current version: $VERSION\nWanted Version: $LIVEZILLA_VERSION\nDoing install/update"
+if [ "$UPGRADE_CHECK" = "notinstalled" ] || [ "$UPGRADE_CHECK" = "upgrade" ]; then
+    if [ "$UPGRADE_CHECK" = "notinstalled" ]; then echo "Not installed\nDoing basic install of version: $LIVEZILLA_VERSION"; fi
+    if [ "$UPGRADE_CHECK" = "upgrade" ]; then echo "Upgrade needed\nUpgrading from version $VERSION to $LIVEZILLA_VERSION"; fi
     echo "Downloading package from $PACKAGE_URL"
     if [ ! -d "/tmp/livezilla-temp" ]; then
         mkdir "/tmp/livezilla-temp"
     fi
     curl -s $PACKAGE_URL -o /tmp/livezilla-temp/livezilla-current.zip
     if [ -f "/tmp/livezilla-temp/livezilla-current.zip" ]; then
-        unzip -o /tmp/livezilla-temp/livezilla-current.zip -d "/tmp/livezilla-temp"
+        unzip -q -o /tmp/livezilla-temp/livezilla-current.zip -d "/tmp/livezilla-temp"
         if [ -d "/tmp/livezilla-temp/livezilla" ]; then
             cp -Rf "/tmp/livezilla-temp/livezilla/." "/var/www/html/"
             chown -R www-data:www-data /var/www/html
@@ -23,8 +25,11 @@ if [ $VERSION != $LIVEZILLA_VERSION ]; then
         echo "File missing - unable to download"
         exit 1
     fi
-else
-    echo "Current version installed: $VERSION\nWanted version: $LIVEZILLA_VERSION\nNothing to do - skipping upgrade"
+elif [ "$UPGRADE_CHECK" = "current" ]; then
+    echo "Installed version is already current: $LIVEZILLA_VERSION"
+elif [ "$UPGRADE_CHECK" = "newer" ]; then
+    echo "Installed version is newer than in Docker Image - Downgrade is not supported\nInstalled: $VERSION\nWanted version: $LIVEZILLA_VERSION"
+    exit 1
 fi
 
 if [ -d "/tmp/livezilla-temp" ]; then
